8c7450e4d29941fb6e71af28c75f20dabee55558
Vadim Zhdanov <zhdanov.vadim.alexandrovich@gmail.com>
2016-01-10 01:19:27 +0200

    first commit

---
+++ src/MainClass.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Вадим on 10.01.2016.
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        int i = 100, j= 200;
+        while(i==1000);
+        System.out.println("Avarage value " + i);
+        System.out.println("hi");
+        System.out.println("hi hi ");
+        System.out.println("hi hi hi ");
+        System.out.println("hi hi hi hi");
+        System.out.println("hi hi hi hi");
+        System.out.println("hi hi hi hi");
+    }
+}
